d66ad4cb441a129a7bef8896d488fda811ed452e
Eremey Malinovsky <Elerphore@gmail.com>
2024-09-20 22:00:26 +0500

    aricle parsing

--- internal/http_client/html_parser/html_parser.go
+++ internal/http_client/html_parser/html_parser.go
@@ -4,29 +4,43 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func Traverse(doc *html.Node) {
-	var traverse func(*html.Node)
+func traverseChildren(n *html.Node) {
 
-	traverse = func(n *html.Node) {
-
-		if n.Data == "h3" && n.Type == html.ElementNode {
-			fmt.Println(n.Data, n.Attr)
-		}
-
-		if n.Type == html.TextNode && n.Parent.Data == "h3" {
-			fmt.Println(n.Data, n.Attr, "\n")
-		}
-
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			traverse(c)
-		}
+	if n.Data == "a" && n.Type == html.ElementNode && strings.Contains(n.Attr[1].Val, "link") {
+		fmt.Println(n.Data, n.Attr[0].Val)
 	}
 
-	traverse(doc)
+	if n.Data == "h3" && n.Type == html.ElementNode {
+		fmt.Println(n.Data, n.Attr)
+	}
+
+	if n.Type == html.TextNode && n.Parent.Data == "h3" {
+		fmt.Println(n.Data, n.Attr)
+	}
+
+	if n.Data == "img" && n.Type == html.ElementNode {
+		fmt.Println(n.Data, n.Attr[0].Val, "\n")
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseChildren(c)
+	}
+}
+
+func Traverse(doc *html.Node) {
+	if doc.Data == "article" {
+		traverseChildren(doc)
+	}
+
+	for c := doc.FirstChild; c != nil; c = c.NextSibling {
+		Traverse(c)
+	}
+
 }
 
 func ParseHTML(body io.ReadCloser) (parsedHTML *html.Node) {
